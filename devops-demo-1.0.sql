0fd357d1d7add5968d20fb5abef268c9c73d5a2c
Ken Erwin <ken.erwin88@gmail.com>
2016-07-12 09:17:57 -0400

    Update

--- devops-demo-1.0.sql
+++ devops-demo-1.0.sql
@@ -1,2 +1,4 @@
 CREATE TABLE dbversion (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,version VARCHAR(30) NOT NULL);
 INSERT INTO dbversion (version) VALUES ('1.0');
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' identified by 'devpass';
+flush PRIVILEGES;
